8f6f0edf5955ee5c1e25c916dd70d92708cc9f4f
Ruben Padilla <rubbenpad2@gmail.com>
2019-11-21 22:56:09 -0500

    Trying the config with components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import logo from '../assets/badge-header.svg';
+
+export const Header = () => {
+  return (
+    <div>
+      <div>
+        <img src={logo} alt="PlatziConf Logo"/>
+      </div>
+    </div>
+  );
+};
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Header } from './components/Header/Header';
+
+const container = document.getElementById('app');
+
+ReactDOM.render(<Header />, container);
