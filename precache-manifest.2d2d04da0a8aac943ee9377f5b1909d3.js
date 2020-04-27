fd129ff6427c42e532e4e802c10230eec767391e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-27 19:20:48 +0000

    Deploy rubbenpad/platzi-badges to github.com/rubbenpad/platzi-badges.git:gh-pages

---
+++ precache-manifest.2d2d04da0a8aac943ee9377f5b1909d3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e59568a80328fa55234ae09469fc761",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27ceaaca9a9218406c3e",
+    "url": "/static/js/2.4b3c43c3.chunk.js"
+  },
+  {
+    "revision": "98704661f5e23aaf8741251516b32453",
+    "url": "/static/js/2.4b3c43c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f97b07b06e5d2ea66179",
+    "url": "/static/js/main.956fb043.chunk.js"
+  },
+  {
+    "revision": "6515e71dbc100ab2d56b",
+    "url": "/static/js/runtime-main.901d1035.js"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/static/media/stars.1d17360a.svg"
+  }
+]);
